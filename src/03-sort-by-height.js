0944f16ff9a6ba3735aa28400aba7d0e21de1c81
agm234 <79218134+agm234@users.noreply.github.com>
2021-04-10 21:35:56 +0300

    feat: add 3

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,20 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const mass = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== -1) {
+      mass.push(arr[i]);
+    }
+  }
+  mass.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      mass.splice(i, 0, -1);
+    }
+  }
+  return mass;
 }
 
 module.exports = sortByHeight;
