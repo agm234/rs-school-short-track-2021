c6edcef1b39e3ac0bcca223d8f5110f498998dcd
agm234 <79218134+agm234@users.noreply.github.com>
2021-04-10 21:50:30 +0300

    feat: add 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,34 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const obj = {};
+  let mass = [];
+  let str1 = '';
+  mass = str.split('');
+  for (let i = 0; i < str.length; i++) {
+    if (mass.includes(str[i]) && (str[i] === str[i + 1] || str[i] === str[i - 1])) {
+      obj[str[i]] = 0;
+    } else if (mass.includes(str[i]) && (str[i] !== str[i + 1] || str[i] !== str[i - 1])) {
+      obj[str[i] + i] = -i;
+    }
+  }
+  for (let i = 0; i < str.length; i++) {
+    Object.keys(obj).forEach((key) => {
+      if (str[i] === key && (str[i] === str[i + 1] || str[i] === str[i - 1])) {
+        obj[key] += 1;
+      }
+    });
+  }
+  Object.keys(obj).forEach((key) => {
+    if (obj[key] > 0) {
+      str1 += obj[key];
+      str1 += key;
+    } else {
+      str1 += key.charAt(0);
+    }
+  });
+  return str1;
 }
 
 module.exports = encodeLine;
